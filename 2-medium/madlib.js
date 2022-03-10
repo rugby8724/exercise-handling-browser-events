1640cc6791c248ed6249f94bfbc83705cbbe5c22
Jeremiah <jeremiah.d.wise@gmail.com>
2022-03-10 16:52:59 -0600

    fix madLib

--- 2-medium/madlib.js
+++ 2-medium/madlib.js
@@ -1,8 +1,11 @@
 const madLibForm = document.getElementById('madLibForm')
 const formGroup = document.getElementsByClassName('form-group')
 const madLibStory = document.getElementById('madLibStory')
+const restartButton = document.getElementById('restart')
 
-// madLibStory.style.display = 'block'
+
+madLibForm.addEventListener('submit', fillBlanks)
+
 
 // madLibForm.addEventListener('submit', function(event) {
 //   event.preventDefault();
@@ -14,107 +17,68 @@ const madLibStory = document.getElementById('madLibStory')
 //   })
 // })
 
+function insertBefore(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode);
+}
+
+
 
 
 const inputList = ['country', 'pluralNoun', 'food', 'device', 'noun', 'adjective']
 
 inputList.forEach((item) => {
+  const p = document.createElement('p')
   const br = document.createElement('br')
   const label = document.createElement('label')
   const input = document.createElement('input')
+  p.classList.add('form-group')
   label.htmlFor = item
   input.setAttribute('type', 'text')
   input.setAttribute('name', item)
   input.id = item
   label.innerText = item
-  formGroup.append(br)
-  formGroup.append(label)
-  formGroup.append(br)
-  formGroup.append(input)
-  
+  p.append(br)
+  p.append(label)
+  p.append(br)
+  p.append(input)
+  p.append(br)  
+  p.after(formGroup)
+  insertBefore(p, madLibForm.lastElementChild)
 })
 
-// function createStory () {
-//   inputList.forEach((item) =>{
-//     const storyItem = document.getElementsByClassName(item)
-//     const input = document.getElementById(item)
-//     storyItem.innerText = input.value
-//   })
+function showForm() {
+  madLibStory.style.display = 'none';
+  madLibForm.style.display = 'block';
+}
 
-// const restartButton = document.querySelector('#restart');
-// const story = document.querySelector('#madLibStory');
-// const form = document.querySelector('#madLibForm');
+function showStory() {
+  madLibStory.style.display = 'block';
+  madLibForm.style.display = 'none';
+}
 
-// function replaceContents(wordClass, inputId) {
-//   const elements = document.getElementsByClassName(wordClass);
-//   const input = document.getElementById(inputId);
+function replaceContents(wordClass, inputId) {
+  const elements = document.getElementsByClassName(wordClass);
+  const input = document.getElementById(inputId);
 
-//   for (let i = 0; i < elements.length; i++) {
-//     const element = elements[i];
-//     element.innerText = input.value;
-//   }
-// }
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    element.innerText = input.value;
+  }
+}
 
-// const inputList = ['country', 'pluralNoun', 'food', 'device', 'noun', 'adjective']
+function fillBlanks(event) {
+  event.preventDefault()
 
-// inputList.forEach((item) => {
-//   const formP = document.getElementsByClassName('form-group')
-//   const br = document.createElement('br')
-//   const label = document.createElement('label')
-//   const input = document.createElement('input')
-//   formP.classList.add('form-group')
-//   label.htmlFor = item
-//   input.setAttribute('type', 'text')
-//   input.setAttribute('name', item)
-//   input.id = item
-//   label.innerText = item
-//   formP.append(br)
-//   formP.append(label)
-//   formP.append(br)
-//   formP.append(input)
-//   formP.append(br)
-  
-// })
+  inputList.forEach((item) => {
+    replaceContents(item, item)
+  })
 
+  showStory()
+}
 
-// function showForm() {
-//   story.style.display = 'none';
-//   form.style.display = 'block';
-// }
-
-// function showStory() {
-//   story.style.display = 'block';
-//   form.style.display = 'none';
-// }
-
-// form.addEventListener('submit', fillBlanks)
-
-// form.addEventListener('submit', function(event) {
-//   event.preventDefault();
-//   replaceContents('animal', 'animal');
-//   replaceContents('country', 'country');
-//   replaceContents('pluralNoun', 'pluralNoun');
-//   replaceContents('food', 'food');
-//   replaceContents('device', 'device');
-//   replaceContents('noun', 'noun');
-//   replaceContents('adjective', 'adjective');
-
-//   showStory()
-// })
-
-// function fillBlanks(event) {
-//   event.preventDefault()
-
-//   inputList.forEach((item) => {
-//     replaceContents(item, item)
-//   })
-
-//   showStory()
-// }
-
-// restartButton.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   showForm()
-//   form.reset();
-// })
+restartButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  showForm()
+  form.reset();
+})
 
