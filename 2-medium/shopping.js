84df47f8edbce6cc7d4ce2d462d48d532ca6301b
Jeremiah <jeremiah.d.wise@gmail.com>
2022-03-10 14:43:19 -0600

    complete shopping list

---
+++ 2-medium/shopping.js
@@ -0,0 +1,30 @@
+const newItemForm = document.getElementById('newItemForm')
+const shoppingList = document.getElementById('shoppingList')
+const itemsInList = shoppingList.getElementsByTagName('li')
+const item = document.getElementById('item')
+
+
+newItemForm.addEventListener('submit', addListItem)
+
+
+
+function addListItem(event){
+  event.preventDefault()
+
+  const newListItem = document.createElement('li')
+  newListItem.innerText = item.value
+  const deleteButton = document.createElement('button')
+  deleteButton.addEventListener('click', removeItem)
+  deleteButton.innerText = 'Delete Me!'
+  newListItem.append(deleteButton)
+  shoppingList.append(newListItem)
+}
+
+function removeItem(){
+  this.closest('li').remove()
+}
+
+
+
+
+
